2a7c39173aaadd3b4ed6f265afb3666e7f0e1077
wangbo <wangbo>
2018-05-07 19:46:37 +0800

    pattern-prototype

---
+++ gupao-pattern/src/main/java/com/gupao/pattern/prototype/simple/CloneTarget.java
@@ -0,0 +1,9 @@
+package com.gupao.pattern.prototype.simple;
+
+public class CloneTarget extends Prototype {
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+}
---
+++ gupao-pattern/src/main/java/com/gupao/pattern/prototype/simple/Prototype.java
@@ -0,0 +1,8 @@
+package com.gupao.pattern.prototype.simple;
+
+public class Prototype implements Cloneable {
+
+    public String name;
+
+    CloneTarget target = null;
+}
