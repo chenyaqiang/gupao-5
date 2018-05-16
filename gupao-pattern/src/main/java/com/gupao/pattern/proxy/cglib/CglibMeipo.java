47a023a55a337c4c4965827f7c102948a1c9b55e
wangbo <wangbo>
2018-05-16 10:13:09 +0800

    pattern-proxy fix

--- gupao-pattern/src/main/java/com/gupao/pattern/proxy/cglib/CglibMeipo.java
+++ gupao-pattern/src/main/java/com/gupao/pattern/proxy/cglib/CglibMeipo.java
@@ -1,5 +1,6 @@
 package com.gupao.pattern.proxy.cglib;
 
+import com.gupao.pattern.proxy.staticed.Person;
 import net.sf.cglib.proxy.Enhancer;
 import net.sf.cglib.proxy.MethodInterceptor;
 import net.sf.cglib.proxy.MethodProxy;
@@ -8,7 +9,7 @@ import java.lang.reflect.Method;
 
 public class CglibMeipo implements MethodInterceptor {
 
-    public Object getInstance(Class<?> clazz) throws Exception{
+    public Object getInstance(Class<? extends Person> clazz) throws Exception {
         Enhancer enhancer = new Enhancer();
         enhancer.setSuperclass(clazz);
         enhancer.setCallback(this);
@@ -22,7 +23,7 @@ public class CglibMeipo implements MethodInterceptor {
         System.out.println("我是媒婆：我要给你找对象，现在已经拿到你的需求");
         System.out.println("开始物色");
 
-        Object result = methodProxy.invokeSuper(o,objects);
+        Object result = methodProxy.invokeSuper(o, objects);
 
         System.out.println("如果合适的话，就准备办事");
         return result;
--- gupao-pattern/src/main/java/com/gupao/pattern/proxy/cglib/ZhangSan.java
+++ gupao-pattern/src/main/java/com/gupao/pattern/proxy/cglib/ZhangSan.java
@@ -1,12 +1,20 @@
 package com.gupao.pattern.proxy.cglib;
 
+import com.gupao.pattern.proxy.staticed.Person;
+
 /**
  * Created by Tom on 2018/3/10.
  */
-public class ZhangSan {
+public class ZhangSan implements Person {
 
+    @Override
     public void findLove(){
         System.out.println("肤白貌美大长腿");
+    }
+
+    @Override
+    public void findJob() {
+
     }
 
 }
